2d714727467adc1fbee763467cec394eab5b31b5
Raikku <107673429+raiccu@users.noreply.github.com>
2024-03-03 19:29:23 +0200

    removed unused import

--- bookstore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import org.springframework.ui.Model;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
